f1497e10ac906ddb1f55bef0a15c3fd56100e291
WesleyQDev <146393663+WesleyQDev@users.noreply.github.com>
2024-02-24 10:00:17 -0300

    Apresentation bug

--- app/components/Apresentation.tsx
+++ app/components/Apresentation.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 export default function Apresentation(){
   return(
     <>
-    <div className=" relative grid h-[40rem] w-full  flex-col items-end py-20 justify-center overflow-hidden  bg-white bg-clip-border text-center text-gray-700">
+    <div className=" relative grid md:h-[40rem] w-full  flex-col items-end py-20 justify-center overflow-hidden  bg-white bg-clip-border text-center text-gray-700">
       <div
         className="bg-fixed	 absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-[url('/games.avif')] bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
         <div className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-t from-black/80 via-black/50"></div>
