f02f358698a1e73e0c287656fb97c44ac71e2b18
WesleyQDev <146393663+WesleyQDev@users.noreply.github.com>
2024-03-02 17:41:42 -0300

    Test

---
+++ app/noctural-ruse/components/Apresentation.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link"
+
+export default function Apresentation(){
+  return(
+    <>
+    <div className=" relative grid rounded flex-col items-end py-32 md:py-20 justify-center overflow-hidden  bg-white bg-clip-border text-center text-gray-700">
+      <div
+        className=" bg-fixed absolute inset-0 m-0 h-full w-full overflow-hidden rounded-none bg-transparent bg-[url('/Fundo.webp')] bg-cover bg-clip-border bg-center text-gray-700 shadow-none">
+        <div className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-t from-black/80 via-black/50"></div>
+      </div>
+      <div className="relative p-6 px-6 py-14 md:px-12">
+        <h2 className="block font-sans text-4xl font-medium leading-[1.5] tracking-normal text-white antialiased">
+         Nocturnal Ruse
+        </h2>
+        <h5 className="block mb-6 font-sans text-xl antialiased font-semibold leading-snug tracking-normal text-gray-400">
+        Game in production
+        </h5>
+        <Link href="/noctural-ruse" className="p-3 m-2 bg-white rounded-full text-neutral-950 uppercase font-bold text-md shadow-amber-50">to participe</Link>
+
+      </div>
+    </div>
+    </>
+  )
+}
---
+++ app/noctural-ruse/page.tsx
@@ -0,0 +1,7 @@
+export default function Home () {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <h1 className="text-3xl font-bold">Nocturnal-Ruse</h1>
+        </div>
+    )
+}
